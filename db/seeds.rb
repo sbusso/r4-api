af734d77e56efb08d4faf02dd761174c63c31073
Steven Yap <stevenyap@futureworkz.com>
2016-05-24 13:13:56 +0800

    Add db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,66 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Message.destroy_all
+User.destroy_all
+Room.destroy_all
+
+peter = User.create!(email:'peter@example.com', password: '123123', token: '')
+mary = User.create!(email:'mary@example.com', password: '123123', token: '')
+henry = User.create!(email:'henry@example.com', password: '123123', token: '')
+cathy = User.create!(email:'cathy@example.com', password: '123123', token: '')
+
+room_mary = Room.create!(receivers: [peter.id, mary.id])
+room_henry = Room.create!(receivers: [peter.id, henry.id])
+room_everyone = Room.create!(receivers: [peter.id, mary.id, henry.id, cathy.id])
+
+[
+  [peter, "Hello Mary"],
+  [mary, "Hello Peter"],
+  [peter, "How are you today?"],
+  [mary, "I am very fine."],
+  [mary, "Thank you for asking, Peter!"],
+].each do |user, message|
+  room = room_mary
+
+  p Message.create!(
+    receivers: room.receivers, 
+    room_id: room.id, 
+    user_id: user.id, 
+    content: message
+  )
+end
+
+[
+  [peter, "Hello Henry"],
+  [henry, "Hello Peter"],
+  [peter, "Where is my lunch?"],
+  [henry, "erm..."],
+  [henry, "Sorry I ate it!!!"],
+  [peter, "you.. you.. you.. :X"],
+].each do |user, message|
+  room = room_henry
+
+  p Message.create!(
+    receivers: room.receivers, 
+    room_id: room.id, 
+    user_id: user.id, 
+    content: message
+  )
+end
+
+[
+  [peter, "Hi I am Peter"],
+  [henry, "Hi I am Henry"],
+  [cathy, "Hi I am Cathy"],
+  [mary, "Hi I am Mary"],
+  [peter, "Did you know Henry ate my lunch?"],
+  [cathy, "That's horrible!"],
+  [henry, "i am really sorry..."],
+].each do |user, message|
+  room = room_everyone
+
+  p Message.create!(
+    receivers: room.receivers, 
+    room_id: room.id, 
+    user_id: user.id, 
+    content: message
+  )
+end
